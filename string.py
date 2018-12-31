2f1a908c3f37b9e7684ad4033e5df6cab5e3b327
xrnurx <nur.rozikin@praweda.id>
2019-01-01 01:22:03 +0700

    mengenal string

---
+++ string.py
@@ -0,0 +1,11 @@
+# Belajar String
+
+nama = "Nur Rozikin"
+kota = 'Depok'
+alamat = 'Jln. Raya'
+
+nama_depan = "Nur"
+nama_belakang = "Rozikin"
+nama_lengkap = nama_depan + " " + nama_belakang
+
+print(nama_lengkap)

f1db420f296b70048dc7de519b16b6ba59c4ede8
xrnurx <nur.rozikin@praweda.id>
2019-01-01 09:41:21 +0700

    belajar For Loop

---
+++ for_loop.py
@@ -0,0 +1,13 @@
+# Belajar For Loop
+
+pelanggan = ["kien", "eko", "budi", "joko", "andi"]
+
+pelanggan.append("diyan")
+pelanggan.append("susi")
+
+# Mengakses semua nama pelanggan ?
+for nama in pelanggan:
+    print("==========================")
+    print(f"Nama Pelanggan : {nama}")
+    print("==========================")
+    

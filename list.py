deb8fb93b63630ef94d969441bb4f177c8f0e9d0
xrnurx <nur.rozikin@praweda.id>
2019-01-01 09:40:27 +0700

    mengenal list di python

---
+++ list.py
@@ -0,0 +1,15 @@
+# Belajar List
+
+nama = ["kien","eko","budi"]
+nama.append("joko")
+
+print(nama[0])
+print(nama[1])
+print(nama[2])
+print(nama[3])
+
+print(len(nama))
+
+nama.remove("budi")
+
+print(nama)

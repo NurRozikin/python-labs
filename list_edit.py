0b811e7f317fb020cdc42ce6da857f7778f6c6d4
xrnurx <nur.rozikin@praweda.id>
2019-01-01 09:41:00 +0700

    belajar list

---
+++ list_edit.py
@@ -0,0 +1,18 @@
+# Membuat List
+nama = ["kien", "eko", "budi"]
+
+# Menambah Data ke List
+nama.append("joko")
+
+# Mengakses List
+nama[0]
+
+# Menghapus Data dari List
+nama.remove("budi")
+
+# Mengubah Data di List
+print(nama)
+
+nama[0] = "ikien"
+
+print(nama)

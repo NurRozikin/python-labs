3fd6e8a7c21b00dd8a73c5ca7d322f899f4472df
xrnurx <nur.rozikin@praweda.id>
2019-01-04 06:15:18 +0700

    belajar method

---
+++ method.py
@@ -0,0 +1,17 @@
+# Belajar membuat Method / Function
+
+nama = []
+
+def print_nama():
+    print("======================")
+    for data in nama:
+        print(data)
+
+
+nama.append("Nur")
+print_nama()
+
+nama.append("Rozikin")
+print_nama()
+
+

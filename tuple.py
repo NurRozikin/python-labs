c64da44d0696ec9674153b6f5e9dd82ef5161e3a
xrnurx <nur.rozikin@praweda.id>
2019-01-04 06:14:42 +0700

    mengenal tuple

---
+++ tuple.py
@@ -0,0 +1,6 @@
+# Belajar Tuple
+# Tuple sama kaya array, cuma tidak bisa di edit hanya bisa diread. 
+# kegunaannya bisa untuk return function
+pelanggan = ("eko", "joko", "andi")
+
+print(pelanggan)

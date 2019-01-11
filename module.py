0d766e6e52ebaec6732e97acf69a57c7476c83a7
xrnurx <nur.rozikin@praweda.id>
2019-01-12 05:50:35 +0700

    belajar module dan menggunakannya

---
+++ module.py
@@ -0,0 +1,13 @@
+# Belajar Module
+
+# 1. import function
+# 2. from function import say_hello/function
+
+from function import say_hello
+from function import total
+
+hello = say_hello("Kien")
+print(hello)
+
+hasil = total(1,2,3,4,5)
+print(hasil)
